a3f4d0c4a500d07ab11eb6f12bfa9c90a0ae0def
juny0 <isjuny0k@gmail.com>
2025-07-16 18:05:28 +0900

    feat(user): User 엔티티 및 UserRepository 생성

---
+++ src/main/java/com/example/blog/entity/User.java
@@ -0,0 +1,22 @@
+package com.example.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")  // 'user'는 예약어라 'users' 사용
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
