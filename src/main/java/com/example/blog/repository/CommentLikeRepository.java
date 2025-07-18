dc38f600f5a296bca05733573fbf40c8ad62b789
juny0 <isjuny0k@gmail.com>
2025-07-18 18:33:21 +0900

    feat: CommentLikeRepository 구현

--- src/main/java/com/example/blog/repository/CommentLikeRepository.java
+++ src/main/java/com/example/blog/repository/CommentLikeRepository.java
@@ -1,4 +1,17 @@
 package com.example.blog.repository;
 
-public class CommentLikeRepository {
+import com.example.blog.entity.Comment;
+import com.example.blog.entity.CommentLike;
+import com.example.blog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+    // 좋아요 여부 확인
+    Optional<CommentLike> findByUserAndComment(User user, Comment comment);
+    // 댓글 좋아요 개수 집계
+    long countByComment(Comment comment);
+    // 좋아요 취소
+    void deleteByUserAndComment(User user, Comment comment);
 }
